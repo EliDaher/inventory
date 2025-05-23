3ca3aec78b8f3201ff11716df0fbe18b75b7f0e6
EliDaher <eeli56315@gmail.com>
2025-05-23 16:48:54 +0300

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/inventory/', // 👈 اسم مجلد مشروعك على GitHub
   plugins: [react()],
 })
